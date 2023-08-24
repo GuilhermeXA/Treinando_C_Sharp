c41eb23ff1790261c6642f42da224fe884dfb64c
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-08-23 22:33:42 -0300

    Criando conversor de moeda mais completo

---
+++ Treinando 23-08-2023_Treinando_Poo_Conversor de moedas completo/ConversorDeMoedas/ConversorDeMoedas/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿public class Calculadora
+{
+    private Dictionary<string, double> taxas;
+
+    public Calculadora()
+    {
+        taxas = new Dictionary<string, double>
+        {
+            { "real", 1.0 },
+            { "dolar", 5.23 },
+            { "euro", 6.17 },
+            { "libra", 7.12 }
+        };
+    }
+
+    public double Converter(double valor, string moedaOrigem, string moedaDestino)
+    {
+        if (taxas.ContainsKey(moedaOrigem) && taxas.ContainsKey(moedaDestino))
+        {
+            double taxaOrigem = taxas[moedaOrigem];
+            double taxaDestino = taxas[moedaDestino];
+
+            double valorEmReal = valor / taxaOrigem;
+            double valorConvertido = valorEmReal * taxaDestino;
+
+            return valorConvertido;
+        }
+        else
+        {
+            throw new ArgumentException("Moedas não suportadas.");
+        }
+    }
+}
---
+++ Treinando 23-08-2023_Treinando_Poo_Conversor de moedas completo/ConversorDeMoedas/ConversorDeMoedas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        Console.WriteLine("Conversor de Moedas");
+
+        Console.Write("Informe o valor: ");
+        double valor = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe a moeda de origem (real, dolar, euro, libra): ");
+        string moedaOrigem = Console.ReadLine().ToLower();
+
+        Console.Write("Informe a moeda de destino (real, dolar, euro, libra): ");
+        string moedaDestino = Console.ReadLine().ToLower();
+
+        try
+        {
+            double valorConvertido = calculadora.Converter(valor, moedaOrigem, moedaDestino);
+            Console.WriteLine($"Valor convertido: {valorConvertido:F2} {moedaDestino}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
