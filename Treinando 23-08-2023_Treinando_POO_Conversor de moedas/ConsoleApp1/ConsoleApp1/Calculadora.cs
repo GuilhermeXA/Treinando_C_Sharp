63fae20adcebe607c28cd234d42192895947c95f
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-08-23 22:18:17 -0300

    Criando conversor de moeda no console usando POO

---
+++ Treinando 23-08-2023_Treinando_POO_Conversor de moedas/ConsoleApp1/ConsoleApp1/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿class Calculadora
+{
+    private double Dolar = 5.23; // Taxa de conversão do Dólar para Real
+    private double Euro = 6.17; // Taxa de conversão do Euro para Real
+    private double Libra = 7.12; // Taxa de conversão da Libra para Real
+
+    public double ConverterDolarParaReal(double valorDolar)
+    {
+        return valorDolar * Dolar;
+    }
+
+    public double ConverterEuroParaReal(double valorEuro)
+    {
+        return valorEuro * Euro;
+    }
+
+    public double ConverterLibraParaReal(double valorLibra)
+    {
+        return valorLibra * Libra;
+    }
+}
---
+++ Treinando 23-08-2023_Treinando_POO_Conversor de moedas/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        Console.WriteLine("Conversor de Moeda");
+        Console.Write("Digite o nome da moeda (dolar/euro/libra): ");
+        string nomeMoeda = Console.ReadLine().ToLower();
+
+        Console.Write("Digite a quantidade da moeda: ");
+        double quantidadeMoeda = double.Parse(Console.ReadLine());
+
+        double resultado = 0;
+
+        switch (nomeMoeda)
+        {
+            case "dolar":
+                resultado = calculadora.ConverterDolarParaReal(quantidadeMoeda);
+                break;
+            case "euro":
+                resultado = calculadora.ConverterEuroParaReal(quantidadeMoeda);
+                break;
+            case "libra":
+                resultado = calculadora.ConverterLibraParaReal(quantidadeMoeda);
+                break;
+            default:
+                Console.WriteLine("Moeda não suportada");
+                return;
+        }
+
+        Console.WriteLine($"O valor em reais é R$: {resultado:N2}");
+    }
+}
