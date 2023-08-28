66b8f269cd5df13d80f7ff1de74c23f1ce965fab
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-08-27 22:30:15 -0300

    Criando calculadora de imc treinando POO

---
+++ Treinando 27-08-2023_Treinando_Poo_IMC/CalculadoraIMC/CalculadoraIMC/CalculadoraIMC.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    class CalculadoraIMC
+    {
+        public string nome { get; set; }
+        public double altura { get; set; }
+        public double peso { get; set; }
+
+        public double CalcularIMC(double altura, double peso)
+        {
+            double resultadoCalculo = peso / (altura * altura);
+            return resultadoCalculo;
+            
+        }
+
+        public string SituacaoIMC(double resultadoCalculo)
+        {
+            if (resultadoCalculo <= 18.5)
+            {
+                return "abaixo do peso";
+            }
+            else if (resultadoCalculo >= 18.6 && resultadoCalculo <= 24.9)
+            {
+                return " no peso ideal";
+            }
+            else if (resultadoCalculo >= 25.0 && resultadoCalculo <= 29.9)
+            {
+                return "acima do peso";
+            }
+            else if (resultadoCalculo >= 30.0 && resultadoCalculo <= 34.9)
+            {
+                return " com obesidade grau 1";
+            }
+            else if (resultadoCalculo >= 35.0 && resultadoCalculo <= 39.9)
+            {
+                return " com obesidade grau 2";
+            }
+            else if (resultadoCalculo >= 40.0)
+            {
+                return " com obesidade grau 3";
+            }
+            else
+            {
+                return "valor de imc inválido";
+            }
+        }
+
+        public string Mensagem()
+        {
+            double resultadoCalculo = CalcularIMC(altura, peso);
+            string situacao = SituacaoIMC(resultadoCalculo);
+            return $"Seu IMC é {resultadoCalculo.ToString("F2")} e você está {situacao}.";
+        }
+    }
+}
---
+++ Treinando 27-08-2023_Treinando_Poo_IMC/CalculadoraIMC/CalculadoraIMC/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CalculadoraIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadoraIMC calculadora = new CalculadoraIMC();
+
+            Console.Write("Digite seu nome: ");
+            calculadora.nome = Console.ReadLine();
+            Console.Write("Digite sua altura: ");
+            calculadora.altura = double.Parse(Console.ReadLine());
+            Console.Write("Digite seu peso: ");
+            calculadora.peso = double.Parse(Console.ReadLine());
+
+            string mensagemIMC = calculadora.Mensagem();
+
+            Console.WriteLine(mensagemIMC);
+
+        }
+    }
+}
