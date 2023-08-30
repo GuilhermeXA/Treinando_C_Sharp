d1ef90274c4cb78ac527185fca7c20a441ecac17
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-08-29 23:00:15 -0300

    Treinando Poo criando programa que calcula idade

---
+++ Treinando 29-08-2023_Treinando_Poo_Classe Pessoas/TreinoPooPessoas/TreinoPooPessoas/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+
+    public int CalcularIdade()
+    {
+        DateTime dataAtual = DateTime.Now;
+        int idade = dataAtual.Year - DataNascimento.Year;
+
+        if (dataAtual.Month < DataNascimento.Month ||
+            (dataAtual.Month == DataNascimento.Month && dataAtual.Day < DataNascimento.Day))
+        {
+            idade--;
+        }
+
+        return idade;
+    }
+}
---
+++ Treinando 29-08-2023_Treinando_Poo_Classe Pessoas/TreinoPooPessoas/TreinoPooPessoas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa();
+
+        Console.Write("Digite o nome da pessoa: ");
+        pessoa.Nome = Console.ReadLine();
+
+        Console.Write("Digite a data de nascimento (yyyy-MM-dd): ");
+        if (DateTime.TryParse(Console.ReadLine(), out DateTime dataNascimento))
+        {
+            pessoa.DataNascimento = dataNascimento;
+
+            int idade = pessoa.CalcularIdade();
+            Console.WriteLine($"{pessoa.Nome} tem {idade} anos.");
+        }
+        else
+        {
+            Console.WriteLine("Data de nascimento inválida.");
+        }
+    }
+}
