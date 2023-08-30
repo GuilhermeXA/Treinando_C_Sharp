98fb0301a99e9efe5711591861a0dde3a99ca064
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-08-29 23:19:34 -0300

    Treinando Poo criando algoritmo de ordenação

---
+++ Treinando 29-08-2023_Treinando_Poo_Ordenação/TreinandoPoo/TreinandoOrdenaçãoUtilizandoPoo/Ordenador.cs
@@ -0,0 +1,34 @@
+﻿class Numero
+{
+    public double Valor { get; }
+
+    public Numero(double valor)
+    {
+        Valor = valor;
+    }
+}
+
+class Ordenador
+{
+    public static void BubbleSort(Numero[] numeros)
+    {
+        int n = numeros.Length;
+        bool trocou;
+
+        do
+        {
+            trocou = false;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (numeros[i].Valor > numeros[i + 1].Valor)
+                {
+                    Numero temp = numeros[i];
+                    numeros[i] = numeros[i + 1];
+                    numeros[i + 1] = temp;
+                    trocou = true;
+                }
+            }
+        } while (trocou);
+    }
+}
---
+++ Treinando 29-08-2023_Treinando_Poo_Ordenação/TreinandoPoo/TreinandoOrdenaçãoUtilizandoPoo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OrdenacaoOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de números: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Numero[] numeros = new Numero[quantidade];
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.Write($"Digite o {i + 1}º número: ");
+                double valor = double.Parse(Console.ReadLine());
+                numeros[i] = new Numero(valor);
+            }
+
+            Ordenador.BubbleSort(numeros);
+
+            Console.WriteLine("Valores ordenados:");
+            foreach (Numero numero in numeros)
+            {
+                Console.WriteLine(numero.Valor);
+            }
+        }
+    }
+}
