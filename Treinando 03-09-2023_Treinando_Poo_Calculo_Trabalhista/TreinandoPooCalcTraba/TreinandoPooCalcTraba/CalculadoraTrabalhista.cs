177f020b702c2922ca694af1e3027a5b0f663429
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-03 18:30:51 -0300

    Criando calculadora simples de cálculo de INSS

---
+++ Treinando 03-09-2023_Treinando_Poo_Calculo_Trabalhista/TreinandoPooCalcTraba/TreinandoPooCalcTraba/CalculadoraTrabalhista.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinandoPooCalcTraba
+{
+    class CalculadoraTrabalhista
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+    
+        public double CalcINSS(double salario)
+        {
+            double valImposto = 0;
+            salario = Salario;
+            if (salario <= 1320)
+            {
+                valImposto = salario * 0.075;
+            }
+            else if (salario <= 2571)
+            {
+                valImposto = salario * 0.09;
+            }
+            else if (salario <= 3856)
+            {
+                valImposto = salario * 0.12;
+            }
+            else if (salario <= 7507)
+            {
+                valImposto = salario * 0.14;
+            }
+            else
+            {
+                valImposto = 0;
+            }
+            return valImposto;
+        }
+    }
+}
---
+++ Treinando 03-09-2023_Treinando_Poo_Calculo_Trabalhista/TreinandoPooCalcTraba/TreinandoPooCalcTraba/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TreinandoPooCalcTraba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadoraTrabalhista conta = new CalculadoraTrabalhista();
+            Console.Write("Digite seu nome: ");
+            conta.Nome = Convert.ToString(Console.ReadLine());
+            Console.Write("Digite seu salário: ");
+            conta.Salario = Convert.ToDouble(Console.ReadLine());
+
+            double valorCalculo = conta.CalcINSS(conta.Salario);
+            Console.WriteLine($"{conta.Nome} o valor do imposto a ser descontado no seu salário é R${valorCalculo.ToString("F2")}");
+        }
+    }
+}
