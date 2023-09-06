4332d38dfdea3feef031c98be7f217470e35ebb5
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-05 22:22:48 -0300

    Criando calculadora simples de precificação em c#

---
+++ Treinando 05-09-2023_Treinando_Poo_Calculadora de Preço/SistemaCalculadoraPreço/SistemaCalculadoraPreço/CalculadoraPreco.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalculadoraPreço
+{
+    class CalculadoraPreco
+    {
+        public string Nome { get; set; }
+        public double CustoFixo { get; set; }
+
+        public double CustoVariavel { get; set; }
+        public double MargemLucro { get; set; }
+
+        public double CalculoDecimalMargemDeLucro(double margemLucro)
+        {
+            this.MargemLucro = margemLucro;
+            double margemDeLucro = margemLucro / 100;
+            return margemDeLucro;
+        }
+
+        public double CalcPreco(double custoFixo, double custoVariavel)
+        {
+            double objetivoMargem = CalculoDecimalMargemDeLucro(MargemLucro);
+            double resultadoObjetivoMargem = (custoFixo + custoVariavel) * objetivoMargem;
+            double preco = custoFixo + custoVariavel + resultadoObjetivoMargem;
+            return preco;
+        }
+    }
+}
---
+++ Treinando 05-09-2023_Treinando_Poo_Calculadora de Preço/SistemaCalculadoraPreço/SistemaCalculadoraPreço/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SistemaCalculadoraPreço
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadoraPreco calculo = new CalculadoraPreco();
+
+            Console.WriteLine("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o custo fixo do produto: ");
+            double custoFixo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o custo variável do produto: ");
+            double custoVariavel = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a margem de lucro desejada: ");
+            double margemLucro = Convert.ToDouble(Console.ReadLine());
+
+            calculo.Nome = nome;
+            calculo.CustoFixo = custoFixo;
+            calculo.CustoVariavel = custoVariavel;
+            calculo.MargemLucro = margemLucro;
+
+            double precoFinal = calculo.CalcPreco(custoFixo, custoVariavel);
+
+            Console.WriteLine("O produto "
+                +calculo.Nome+" com o custo fixo de R$"
+                +calculo.CustoFixo.ToString("F2")+" e com o custo variável R$"
+                +calculo.CustoVariavel.ToString("F2")+" e com o seu objetivo de margem de lucro de "
+                +calculo.MargemLucro.ToString("F2")+"% fica com seu preço final de R$"
+                +precoFinal.ToString("F2"));
+        }
+    }
+}
