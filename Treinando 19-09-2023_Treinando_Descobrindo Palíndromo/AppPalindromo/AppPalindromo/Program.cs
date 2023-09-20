434850d205351e8ec3b0af84ea38a7525d5bf4fc
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-19 22:08:57 -0300

    App Verificando se uma string é palíndromo ou não

---
+++ Treinando 19-09-2023_Treinando_Descobrindo Palíndromo/AppPalindromo/AppPalindromo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AppPalindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a palavra: ");
+            string original = Console.ReadLine();
+            string invertida = InverterString(original);
+
+            if (original == invertida)
+            {
+                Console.WriteLine("A palavra " + original + " é um palíndromo!");
+            }
+            else
+            {
+                Console.WriteLine("A palavra "+original+" não é um palíndromo!");
+            }
+        }
+        static string InverterString(string input)
+        {
+            char[] caracteres = input.ToCharArray();
+            Array.Reverse(caracteres);
+            return new string(caracteres);
+        }
+    }
+}
