92e077c043cd25b5d05e8d031d49374069227843
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-20 21:54:42 -0300

    Fazendo Programa usando lista

---
+++ Treinando 20-09-2023_Treinando_Listas_Pares/TreinandoCSharp_Listas/TreinandoCSharp_Listas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> valores = new List<int>();
+        List<int> pares = new List<int>();
+
+        Console.WriteLine("Digite uma série de números inteiros (digite um número negativo para encerrar):");
+
+        while (true)
+        {
+            string entrada = Console.ReadLine();
+            int valor;
+
+            if (int.TryParse(entrada, out valor))
+            {
+                if (valor < 0)
+                {
+                    break; // Encerra o loop quando um número negativo é digitado
+                }
+
+                valores.Add(valor);
+
+                if (valor % 2 == 0)
+                {
+                    pares.Add(valor);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Digite um número inteiro.");
+            }
+        }
+
+        Console.WriteLine("Valores digitados:");
+        foreach (int numero in valores)
+        {
+            Console.Write(numero + " ");
+        }
+
+        Console.WriteLine("\nNúmeros pares:");
+        foreach (int numero in pares)
+        {
+            Console.Write(numero + " ");
+        }
+    }
+}
