f6a90053cd1310ffeafdf093931e89db1563bd53
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-21 23:16:42 -0300

    Treinando estrutura switch case

---
+++ Treinando 21-09-2023_Treinando_Switch/TreinandoSwitch/TreinandoSwitch/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace TreinandoSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("A variável tem o valor 1");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("A variável tem o valor 2");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("A variável tem o valor 3");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("A variável tem o valor 4");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("A variável tem o valor 5");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("A variável tem o valor 6");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("A variável tem o valor default");
+                        break;
+                    }
+            }
+            Console.WriteLine(numero);
+        }
+    }
+}
