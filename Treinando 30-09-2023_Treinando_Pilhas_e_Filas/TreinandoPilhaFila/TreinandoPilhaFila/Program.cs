46fbcc221b3154e6149f4b67e826491aa25008cd
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-30 22:19:15 -0300

    Treinando Pilhas e Filas

---
+++ Treinando 30-09-2023_Treinando_Pilhas_e_Filas/TreinandoPilhaFila/TreinandoPilhaFila/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TreinandoPilhaFila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pilha e Fila:
+            Stack<string> pilha = new Stack<string>();
+            pilha.Push("A");
+            pilha.Push("B");
+            string topoDaPilha = pilha.Pop();  // Remove e retorna o elemento no topo da pilha
+
+            Queue<int> fila = new Queue<int>();
+            fila.Enqueue(1);
+            fila.Enqueue(2);
+            int primeiroDaFila = fila.Dequeue();  // Remove e retorna o primeiro elemento da fila
+
+        }
+    }
+}
