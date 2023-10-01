889c8f4f355ca775b8730b972a66d28e89d46dfb
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-30 22:18:47 -0300

    Treinando Dicionários

---
+++ Treinando30-09-2023_Treinando_Dicionários/TreinandoDicionários/TreinandoDicionários/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TreinandoDicionários
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dicionário:
+            Dictionary<string, int> idadePorNome = new Dictionary<string, int>();
+
+            idadePorNome["Alice"] = 25;  // Adiciona uma entrada ao dicionário
+            int idadeDoBob = idadePorNome["Bob"];  // Obtém o valor associado à chave "Bob"
+
+        }
+    }
+}
