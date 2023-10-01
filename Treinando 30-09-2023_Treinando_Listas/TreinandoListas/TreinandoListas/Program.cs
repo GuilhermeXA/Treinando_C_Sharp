3d734401e6f710879adeab2326a4622dec27929b
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-30 22:17:52 -0300

    Treinando Listas

---
+++ Treinando 30-09-2023_Treinando_Listas/TreinandoListas/TreinandoListas/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TreinandoListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração e Inicialização:
+            List<int> numeros = new List<int>(); // Lista vazia de inteiros
+
+            List<string> nomes = new List<string>
+            {
+                "Alice",
+                "Bob",
+                "Charlie"
+            };  // Inicialização com valores
+
+            //Adição e Remoção de Elementos:
+            numeros.Add(10);  // Adiciona o número 10 à lista
+            numeros.AddRange(new List<int> { 20, 30, 40 });  // Adiciona uma coleção à lista
+
+            nomes.Remove("Bob");  // Remove o elemento "Bob" da lista
+
+            //Acesso aos Elementos:
+            int primeiroNumero = numeros[0];  // Acessa o primeiro elemento da lista
+
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);  // Iteração sobre os elementos da lista
+            }
+        }
+    }
+}
