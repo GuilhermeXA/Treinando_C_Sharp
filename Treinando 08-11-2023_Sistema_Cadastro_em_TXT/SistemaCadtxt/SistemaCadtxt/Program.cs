f972871fe964e777601e1c541e7b5ea9a1880530
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-11-08 22:46:26 -0300

    Sistema de cadastro simples registro em .txt

---
+++ Treinando 08-11-2023_Sistema_Cadastro_em_TXT/SistemaCadtxt/SistemaCadtxt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace SistemaCadtxt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite o nome (ou digite 'sair' para sair): ");
+                string nome = Console.ReadLine();
+
+                if (nome.ToLower() == "sair")
+                    break;
+
+                Console.WriteLine("Digite o endereço: ");
+                string endereco = Console.ReadLine();
+
+                Console.WriteLine("Digite o telefone: ");
+                string telefone = Console.ReadLine();
+
+                Console.WriteLine("Digite o email: ");
+                string email = Console.ReadLine();
+
+                string registro = $"{nome}, {endereco}, {telefone}, {email}";
+
+                // Escreve o registro no arquivo de texto
+                using (StreamWriter file = new StreamWriter("registros.txt", true))
+                {
+                    file.WriteLine(registro);
+                }
+
+                Console.WriteLine("Registro adicionado!");
+            }
+        }
+    }
+}
